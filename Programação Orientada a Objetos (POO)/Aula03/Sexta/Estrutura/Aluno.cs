54014da27944ad3c8d35324dd4301067aa9fe244
Marky-Wuz <lummesshopp@gmail.com>
2026-08-14 19:24:53 -0300

    Aula 02 sobre OOP

---
+++ Programação Orientada a Objetos (POO)/Aula03/Sexta/Estrutura/Aluno.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Estrutura
+{
+    internal struct Aluno
+    {
+        //Membros
+        //Campos
+        public string nome;
+        public int cpf;
+        public double altura;
+
+        public Aluno(string nome, int cpf) : this()
+        {
+            this.nome = nome;
+            this.cpf = cpf;
+        }
+
+        // Construtor
+        public Aluno(string nome, int cpf, double altura)
+        {
+            this.nome = nome;
+            this.cpf = cpf;
+            this.altura = altura;
+        }
+
+
+            //Métodos
+            public double idade(int idade)
+        {
+            return idade / 365;
+        }
+    }
+}
