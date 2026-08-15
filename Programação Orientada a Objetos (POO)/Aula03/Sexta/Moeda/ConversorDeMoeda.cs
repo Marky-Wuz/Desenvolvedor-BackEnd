d7535fbdc4ab5d9301bf106e54f3c2053f2bc1a0
Marky-Wuz <lummesshopp@gmail.com>
2026-08-14 21:10:01 -0300

    Conversor de moeda, Classes estaticas e Calculadora (Aula Dev BackEND, SENAI: SEXTA | Aula03 OOP)

---
+++ Programação Orientada a Objetos (POO)/Aula03/Sexta/Moeda/ConversorDeMoeda.cs
@@ -0,0 +1,15 @@
+﻿namespace Moeda
+{
+    public static class ConversorDeMoeda
+    {
+
+        public const double IOF = 0.06;
+
+        public static double Comprar(double cotação, double compra)
+        {
+           
+            double pagamento = (cotação * compra * (1 + IOF));
+            return pagamento;
+        }
+    }
+}
---
+++ Programação Orientada a Objetos (POO)/Aula03/Sexta/Moeda/Program.cs
@@ -0,0 +1,12 @@
+﻿//Declaração de variaveis
+using Moeda;
+
+double compra, cotação;
+
+// entrada de dados
+Console.Write("Digite a cotação do Dólar: ");
+cotação = double.Parse(Console.ReadLine());
+Console.Write("Quantos dólares você irá comprar? ");
+compra = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"Valor á ser pago em reais = {ConversorDeMoeda.Comprar(cotação, compra):f2}");
